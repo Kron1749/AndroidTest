fc46510bb7fdd1c018f4d79022d79b8a761a28af
Kron1749 <drakgr98@gmail.com>
2021-03-01 11:10:57 +0300

    Creating first unit tests

---
+++ app/src/main/java/com/app/androidtest/RegistrationUtil.kt
@@ -0,0 +1,26 @@
+package com.app.androidtest
+
+object RegistrationUtil {
+
+    private val existingUsers = listOf("Peter", "Carl")
+
+    fun validateRegistrationInput(
+        username: String,
+        password: String,
+        confirmedPassword: String
+    ): Boolean {
+        if(username.isEmpty() || password.isEmpty()) {
+            return false
+        }
+        if(username in existingUsers) {
+            return false
+        }
+        if(password != confirmedPassword) {
+            return false
+        }
+        if(password.count { it.isDigit() } < 2) {
+            return false
+        }
+        return true
+    }
+}
