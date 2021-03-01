fadb1308fe06450aced611bc671a781ff64a45d6
Kron1749 <drakgr98@gmail.com>
2021-03-01 11:58:08 +0300

    Creating DB

---
+++ app/src/main/java/com/app/androidtest/data/local/ShoppingItemDatabase.kt
@@ -0,0 +1,13 @@
+package com.app.androidtest.data.local
+
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [ShoppingItem::class],
+    version = 1
+)
+abstract class ShoppingItemDatabase:RoomDatabase() {
+    abstract fun shoppingDao():ShoppingDao
+}
