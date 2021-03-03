0762d8b9cf18a161c6b1f41088f1b81cc370f4b2
Kron1749 <drakgr98@gmail.com>
2021-03-03 16:08:31 +0300

    Test Swipe commit

--- app/src/androidTest/java/com/app/androidtest/ui/ShoppingFragmentTest.kt
+++ app/src/androidTest/java/com/app/androidtest/ui/ShoppingFragmentTest.kt
@@ -1,41 +1,77 @@
 package com.app.androidtest.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeLeft
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
 import com.app.androidtest.R
+import com.app.androidtest.adapters.ShoppingItemAdapter
+import com.app.androidtest.data.local.ShoppingItem
+import com.app.androidtest.getOrAwaitValue
 import com.app.androidtest.launchFragmentInHiltContainer
+import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
+import org.mockito.Mockito.*
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
 @ExperimentalCoroutinesApi
-class ShoppingFragmentTest{
+class ShoppingFragmentTest {
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var testFragmentFactory: TestShoppingFragmentFactory
+
     @Before
-    fun setup(){
+    fun setup() {
         hiltRule.inject()
     }
 
     @Test
-    fun clickAddShoppingItemButton_navigateToAddShoppingItemFragment(){
+    fun swipeShoppingItem_deleteItemInDb() {
+        val shoppingItem = ShoppingItem("TEST", 1, 1f, "TEST", 1)
+        var testViewModel: ShoppingViewModel? = null
+        launchFragmentInHiltContainer<ShoppingFragment>(
+            fragmentFactory = testFragmentFactory
+        ) {
+            testViewModel = viewModel
+            viewModel?.insertShoppingItemIntoDb(shoppingItem)
+        }
+
+        onView(withId(R.id.rvShoppingItems)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<ShoppingItemAdapter.ShoppingItemViewHolder>(
+                0,
+                swipeLeft()
+            )
+        )
+
+        assertThat(testViewModel?.shoppingItems?.getOrAwaitValue()).isEmpty()
+    }
+
+    @Test
+    fun clickAddShoppingItemButton_navigateToAddShoppingItemFragment() {
         val navController = mock(NavController::class.java)
-        launchFragmentInHiltContainer<ShoppingFragment> {
-            Navigation.setViewNavController(requireView(),navController)
+
+        launchFragmentInHiltContainer<ShoppingFragment>(
+            fragmentFactory = testFragmentFactory
+        ) {
+            Navigation.setViewNavController(requireView(), navController)
         }
 
         onView(withId(R.id.fabAddShoppingItem)).perform(click())
@@ -44,6 +80,4 @@ class ShoppingFragmentTest{
             ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment()
         )
     }
-
-
 }
